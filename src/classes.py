71742a27f6d33ee1d24588846458bf1ba8aee4ce
Hubert Przeor <s242914@dtu.dk>
2025-12-01 12:59:35 +0100

    v2 model1

--- src/classes.py
+++ src/classes.py
@@ -1,4 +1,6 @@
 import pandas as pd
+from math import isclose
+
 class Substation:
     def __init__(self, id, node, capacity, neighbors, fix_cost = 0, edge_cost = 0):
         self.id = id
@@ -6,15 +8,17 @@ class Substation:
         self.capacity = capacity
         self.neighbors = neighbors  # Nodes with potential connection
         self.fix_cost = fix_cost    # Fixed cost for activation
-        self.edge_cost = edge_cost  # Cost for activating arc connection
+        self.edge_cost = edge_cost  # Cost for activating arc connection (feeder lines)
 
 class Heatpump():
+    """Potential class to utilize"""
     def __init__(self, id, node, load):
         self.id = id
         self.node = node
         self.load = load
 
 class EV():
+    """Potential class to utilize"""
     def __init__(self, id, node, load):
         self.id = id
         self.node = node
@@ -29,47 +33,50 @@ class DistributionNetwork():
         LOADS (list): List of existing system nodes
         SUBSTATIONS (list): List of existing substations (Substation objects).
         load_capacity (dict): Loads capacity (P^D_i)
-        substation_capacity (dict): Substation capacity
+        nodes_connected (dict):
+        loads_locations (dict):
+        line_cost (float): Default cost of building network lines (different from substation feeder lines)
 
     Methods:
         add_candidate_substations:
         update_arcs:
-        update_substations:
+        update_initial_conditions:
     """
-    def __init__(self, NODES, LOADS, SUBSTATIONS, load_capacity, nodes_connected, loads_locations):
+    def __init__(self, NODES, LOADS, SUBSTATIONS, load_capacity, nodes_connected, loads_locations, line_cost):
         self.NODES = NODES
         self.LOADS = LOADS
         self.SUBSTATIONS = SUBSTATIONS
         self.load_capacity = load_capacity
         self.nodes_connected = nodes_connected
         self.loads_locations = loads_locations
+        self.line_cost = line_cost  # 
 
         # Arcs (distribution lines)
         self.update_arcs()
-        self.edge_cost = {e: 0 for e in self.E} # Cost of initial lines set to 0
+        self.edge_cost = {e: 0 for e in self.E} # Initial condition for "existing lines". Cost of initial lines set to 0
+        self.A_activated = [] # *Only for MODEL 1: List of activated distribution lines from previous time periods 
 
         self.mapping_substations = pd.DataFrame()
         for S in self.SUBSTATIONS:
             self.mapping_substations.loc[S.id, self.NODES] = 0
             self.mapping_substations.loc[S.id, S.node] = 1
 
-        #branch_capacity: pd.DataFrame
-        #bus_susceptance: pd.DataFrame
-        #slack_bus: str
 
     def add_candidate_substations(self, substations):
-        """Adds candidate substations for expansion."""
+        """Adds candidate substations for expansion. 
+        Substations can't be connected to each other.
+        """
         if not isinstance(substations, list):
             substations = [substations]
         
         for S in substations:
             self.SUBSTATIONS.append(S)
             self.NODES.append(S.node)
-            self.nodes_connected[S.node] = S.neighbors
+            self.nodes_connected[S.node] = S.neighbors        # Adding connections to neighbors in the new node
             
             self.update_arcs()
             for neighbor in S.neighbors:
-                self.nodes_connected[neighbor].append(S.node) # Also adding connection in nodes that existed previously
+                self.nodes_connected[neighbor].append(S.node) # Also adding connection in neighboring nodes that existed before
 
                 a = int(S.node[1:])             # Index (integer) of the node
                 b = int(neighbor[1:])           # Index (integer) of the neighboring node
@@ -77,8 +84,9 @@ class DistributionNetwork():
                 self.edge_cost[e] = S.edge_cost # Setting edge cost to that arc
 
     def add_appliances(self, appliances):
+        """Method suggestion for future models."""
         if not isinstance(appliances, list):
-            appliances = []
+            appliances = [appliances]
 
     def update_arcs(self):
         # Undirected edges
@@ -96,14 +104,47 @@ class DistributionNetwork():
             self.A.append((i,j))
             self.A.append((j,i))
 
-    def update_substations(self, w: dict):
-        """Used in the deterministic (single-time) model to switch candidate substations 
-        to on-line (mark them as existing for the initial constraint).
-        Attributes:
-        w (dict): Dictionary of substations activation states from previous solution.
+    def update_initial_conditions(self, w: dict, x: dict, z: dict):
         """
+        Used in Model 1 (single-period optimization).
+
+        Attributes:
+            w (dict): Dictionary of substations activation variables from previous solution.
+        """
+        # --- Substations ---
+        # Mark activated substations from a previous time period as on-line in the next time period. 
         for s, state in w.items():
             # s = [1,2, ...] starts with 1
             if state == 1 and self.SUBSTATIONS[s-1].fix_cost != 0 and self.SUBSTATIONS[s-1].edge_cost != 0:
                 self.SUBSTATIONS[s-1].fix_cost = 0
                 self.SUBSTATIONS[s-1].edge_cost = 0
+
+        # --- Distribution lines ---
+        # Every line coming from a newly activated substation will now always be active 
+        # (aka you cant disconnect a built substation)
+        for (i,j,s), state in x.items():
+            e = ((min(i,j), max(i,j)))
+            if state == 1 and self.edge_cost[e] != 0:
+                self.edge_cost[e] = 0                           # Changes edge cost to 0
+                self.A_activated.append((i,j,s))                # Adds edge to A_activated so its always active from now
+
+        # Every inactive line, should now have a cost.
+        # After first initialization all "existing" lines had 0 cost, so they could be freely build by a solver.
+        # But after first year, if there are any inactive lines among those that were initially existing,
+        # meaning there are gaps now (due to new substations active in the network), REBUILDING those lines
+        # should cost. This is done so the solver does not freely jump assigning different paths ("portioning demands")
+        # each time without any cost.
+
+        for (i,j) in self.E:
+            state_sum = 0
+            for s in range(1,len(self.SUBSTATIONS)+1,1):
+                state = x[(i,j,s)] + x[(j,i,s)] # Look for disactivation of an arc in any direction (i,j) or (j,i)
+                state_sum += state
+            
+            if isclose(state_sum, 0) and self.edge_cost[(i,j)] == 0:
+                print("Arc disconnected:", (i,j))
+                self.edge_cost[(i,j)] = self.line_cost
+
+        # Reinforced existing substations (increased capacity)
+        for s, multiplier in z.items():
+            self.SUBSTATIONS[s-1].capacity += multiplier * 10   # adds (multiplier)*10 to capacity
--- src/solver.py
+++ src/solver.py
@@ -4,12 +4,24 @@ from gurobipy import GRB, quicksum
 
 from src.classes import DistributionNetwork
 
-def solve_network(Network: DistributionNetwork, OutputFlag=0):
+def solve_network(Network: DistributionNetwork, B, dr, Y, OutputFlag=0):
+    """Solves a Distribution Network.
+
+    Args:
+        Network (DistributionNetwork): Defined Distribution Network
+        B (float): Annual budget
+        dr (float): Discount rate
+        Y (int): Year (ONLY FOR MODEL 1). Used for cost discounting.
+        OutputFlag (int, optional): _description_. Defaults to 0.
+
+    Returns:
+        dict: Dictionary with values of objective function and decision variables.
+    """
     # ---------------
-    #  Index sets
+    #  Index sets: Utilized in later calculations
     # ---------------
-    N = list(np.arange(1, len(Network.NODES)+1))        # List of node indices
-    S = list(np.arange(1, len(Network.SUBSTATIONS)+1))  # List of substations indices
+    N = list(np.arange(1, len(Network.NODES)+1))        # List of node indices starting from 1 [1, 2, ...]
+    S = list(np.arange(1, len(Network.SUBSTATIONS)+1))  # List of substations indices starting from 1 [1, 2, ...]
 
     # Demand vector (demand of each node)
     d = np.zeros(len(Network.NODES))
@@ -29,52 +41,67 @@ def solve_network(Network: DistributionNetwork, OutputFlag=0):
     model = gp.Model("Radial_Distribution_Network")
 
     #   Model. Parameters
-    M = float(np.sum(d)) # Big-M for flow
+    M = float(np.sum(d)) # Big-M used to constraint power flow.
+    C_R = 200 # Cost of capacity reinforcement (assume equal for every substation)
+    R = 10 # Size of a single capacity reinforcement
 
     #   Model. Decision variables
     w = model.addVars(S, vtype=GRB.BINARY, name="w")
     y = model.addVars([(i,s) for i in N for s in S], vtype=GRB.BINARY, name="y")
     x = model.addVars([(i,j,s) for (i,j) in Network.A for s in S], vtype=GRB.BINARY, name="x")
+    z = model.addVars(S, lb=0, vtype=GRB.INTEGER, name="z") # NEW
     f = model.addVars([(s,i,j) for s in S for (i,j) in Network.A], lb=0.0, ub=M, vtype=GRB.CONTINUOUS, name="f")
     r = model.addVars(S, lb=0.0, vtype=GRB.CONTINUOUS, name="r")
 
     #   Model. Constraints
-    #
-    # Power balance
+    #   (1) Power balance
+    #   All power flows sum up to total system demand (100% covered)
     model.addConstr(quicksum(r[s] for s in S) == np.sum(d), name="power_balance")
 
-    # Node assignment: only demand nodes
+    #   (2) Non-substation node assignment
+    #   Non-substation node must be assigned to one, and only one, substation
     for i in D:
         model.addConstr(quicksum(y[i,s] for s in S) == 1, name=f"assign_{i}")
 
+    #   Node assigned to a substation y[i,s] = 1 only if substation is active w[s] = 1
     for i in N:
         for s in S:
             model.addConstr(y[i,s] <= w[s], name=f"y_le_w_{i}_{s}")
 
-    # Substation node assignment: assigned to self if activated
+    #   (3) Substation node assignment: assigned to self if activated
     for s in S:
         node_idx = int(Network.SUBSTATIONS[s-1].node[1:])
         model.addConstr(y[node_idx,s] == w[s], name=f"substation_assign_{s}")
-        # substation nodes cannot be assigned to other substations
+        # Substation nodes cannot be assigned to other substations
         for s2 in S:
             if s2 != s:
                 model.addConstr(y[node_idx,s2] == 0, name=f"substation_no_assign_{s}_{s2}")
 
-    # Existing substations must be active (Initial constraint)
-    # Looks for substations with ZERO FIXED COST.
+    #   (4 ) Initial Constraints
+    #   (4a) Existing substations activation
+    #   EXISTING substations must stay active. (Looks for substations with ZERO FIXED COST)
     for s in S:
         if Network.SUBSTATIONS[s-1].fix_cost == 0:
             model.addConstr(w[1] == 1, name="w_act_1")
 
-    # Substation supply
+    #   (4b) Distribution lines activation
+    #   If a substation was activated, its feeder line is forced to stay active
+    #   Thinking about deleting this one...
+    for (i,j,s) in Network.A_activated:
+        model.addConstr(x[i,j,s] == 1, name="x_act")
+        
+    #   (5) Substation supply
+    #   Power flow from a substation is equal to demands supplied by this substation
     for s in S:
         model.addConstr(r[s] == quicksum(d[i-1]*y[i,s] for i in N), name=f"supply_def_{s}")
 
-    # Capacity
+    #   (6) Capacity constraint
+    #   Power flow from a substation lower or equal to substation's max capacity + potential capacity reinforcements
     for s in S:
-        model.addConstr(r[s] <= Network.SUBSTATIONS[s-1].capacity * w[s], name=f"capacity_{s}")
+        model.addConstr(r[s] <= (Network.SUBSTATIONS[s-1].capacity + R * z[s]) * w[s], name=f"capacity_{s}") #NEW
+        #model.addConstr(r[s] <= (Network.SUBSTATIONS[s-1].capacity) * w[s], name=f"capacity_{s}") #OLD
 
-    # Flow conservation
+    #   (7) Flow conservation
     for s in S:
         for i in N:
             incoming = quicksum(f[s,j,i] for (j,k) in Network.A if k==i)
@@ -84,29 +111,34 @@ def solve_network(Network: DistributionNetwork, OutputFlag=0):
             elif i in D:
                 model.addConstr(outgoing - incoming == -d[i-1]*y[i,s], name=f"flow_balance_{s}_{i}")
 
-    # Flow only if arc assigned
+    #   (8) Flow only if arc assigned
+    #   Flow through arc is costrained by total system demand. Equal to zero if arc is inactive 
     for s in S:
         for (i,j) in Network.A:
             model.addConstr(f[s,i,j] <= M*x[i,j,s], name=f"f_cap_{s}_{i}_{j}")
 
-    # Radiality: each demand node has exactly one parent per assigned substation
+    #   (9) Radiality: each non-substation node has exactly one parent per assigned substation
     for s in S:
         for i in D:
             model.addConstr(quicksum(x[j,i,s] for (j,k) in Network.A if k==i) == y[i,s], name=f"one_parent_{s}_{i}")
-        # substation node has no parent
+        # Substation node has no parent
         node_idx = int(Network.SUBSTATIONS[s-1].node[1:])
         model.addConstr(quicksum(x[j,node_idx,s] for (j,k) in Network.A if k==node_idx) == 0, name=f"parent_root_{s}")
 
-    # Tree size: arcs = nodes assigned - w[s]
+    #   (10) Tree size: arcs = nodes assigned - w[s]
     for s in S:
         model.addConstr(quicksum(x[i,j,s] for (i,j) in Network.A) == quicksum(y[k,s] for k in N) - w[s], name=f"tree_size_{s}")
 
-    #   Model. Objective function
-    # 
-    #   f(x) = fixed cost + edge cost
+    #   (11) Annual budget costraint
     fix_term = quicksum(Network.SUBSTATIONS[s-1].fix_cost * w[s] for s in S)
-    edge_term = 0.5*quicksum(Network.edge_cost[(min(i,j),max(i,j))]*x[i,j,s] for (i,j) in Network.A for s in S)
-    model.setObjective(fix_term + edge_term, GRB.MINIMIZE)
+    edge_term = quicksum(Network.edge_cost[(min(i,j),max(i,j))]*x[i,j,s] for (i,j) in Network.A for s in S)
+    reinf_term = quicksum(C_R * z[s] for s in S)
+    model.addConstr(fix_term + edge_term + reinf_term <= B, name=f"budget_{Y}")
+
+    #   Model. Objective function
+    #   min f(x) = substation cost + feeder cost + capacity reinforcement cost
+
+    model.setObjective((fix_term + edge_term + reinf_term)/(1+dr)**(Y-1), GRB.MINIMIZE)
 
     # ------------------------
     #   Solve
@@ -114,27 +146,34 @@ def solve_network(Network: DistributionNetwork, OutputFlag=0):
     model.setParam('OutputFlag', OutputFlag) # Decide if to display the optimization output (Def. = 0)
     model.optimize()
 
-    # Extract numerical results
-    w_val = {s: w[s].X for s in S}
-    y_val = {(i, s): y[i, s].X for i in N for s in S}
-    x_val = {(i, j, s): x[i, j, s].X for (i, j) in Network.A for s in S}
-    f_val = {(s, i, j): f[s, i, j].X for s in S for (i, j) in Network.A}
-    r_val = {s: r[s].X for s in S}
+    # Extract numerical results (if feasible)
+    if model.status == GRB.INFEASIBLE:
+        raise Exception("Model is infeasible. Check budget constraints.")
+    else:
+        w_val = {s: w[s].X for s in S}
+        y_val = {(i, s): y[i, s].X for i in N for s in S}
+        x_val = {(i, j, s): x[i, j, s].X for (i, j) in Network.A for s in S}
+        f_val = {(s, i, j): f[s, i, j].X for s in S for (i, j) in Network.A}
+        r_val = {s: r[s].X for s in S}
+        z_val = {s: z[s].X for s in S}
 
     # Return numerical data
     return {
-        "objective": model.ObjVal,
-        "w": w_val,
-        "y": y_val,
-        "x": x_val,
-        "f": f_val,
-        "r": r_val
-    }
+            "objective": model.ObjVal,
+            "w": w_val,
+            "y": y_val,
+            "x": x_val,
+            "f": f_val,
+            "r": r_val,
+            "z": z_val
+            }
 
 
 def print_results(Network, solution):
     N = list(np.arange(1, len(Network.NODES)+1))  # [1,2,3,4]
     S = list(np.arange(1, len(Network.SUBSTATIONS)+1))  # [1,2]
+
+    print("\nTotal cost:", solution['objective'])
 
     print("\nSubstation activation and supply:")
     for s in S:
